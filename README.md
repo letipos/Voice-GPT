a1709d0e5efa925df9561f6b4589303337e4e785
Leonard Posner <Leonard.Timo.Posner@ibm.com>
2023-03-20 12:21:35 +0100

    update README and provided comments in code

--- README.md
+++ README.md
@@ -21,3 +21,37 @@ This is a Python program that allows users to interact with an AI chatbot using 
 
 ## Acknowledgements
 This program was developed with the help of OpenAI's GPT-3 language model, the `pyttsx3` text-to-speech library, and the `speech_recognition` and `pyaudio` speech recognition libraries.
+
+# ChatGPT Voice Bot with OpenAI and Gradio
+
+This project implements a voice-based chatbot using OpenAI's GPT-3 API and Gradio's user interface. The chatbot is capable of recognizing and transcribing voice inputs, generating responses to those inputs using GPT-3, and delivering those responses through a text-based user interface.
+
+## Requirements
+- OpenAI API key
+- Python 3.x
+- `openai`, `gradio`, `pyttsx3`, `speech_recognition`, and `pyaudio` Python packages
+
+## Usage
+
+To get started with this project, follow these steps:
+
+1. Clone the repository to your local machine using `git clone https://github.com/YOUR-USERNAME/YOUR-REPOSITORY`.
+2. Install the required dependencies by running `pip install -r requirements.txt`.
+3. Set your OpenAI API key by adding `export OPENAI_API_KEY=yourkeyhere` to your environment variables, replacing "yourkeyhere" with your actual OpenAI API key.
+4. Run the application by running `python app.py`.
+5. Open the application in your web browser by navigating to `http://localhost:7860`.
+
+## Usage
+
+Once the application is running, you can use it to have a conversation with the chatbot. Follow these steps to start a conversation:
+
+1. Click the "Record audio" button to begin recording your voice input.
+2. Speak your message into the microphone.
+3. Click the "Stop recording" button to stop recording your voice input.
+4. Wait for the chatbot to generate a response.
+
+You can clear the chat history at any time by clicking the "Clear" button.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- chatbot.py
+++ chatbot.py
@@ -1,33 +1,30 @@
+# Import required libraries
 import openai
 import pyttsx3
 import speech_recognition as sr
 import os
 import gradio as gr
-import configparser
-import random
-import time 
 
-
-
-# Set the API key with config file
+# Set the OpenAI API key
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-
+# Initialize text-to-speech engine
 engine = pyttsx3.init()
 
-
+# Define start and restart sequences for the chatbot prompt
 start_sequence = "\nAI:"
 restart_sequence = "\nHuman: "
 prompt = "The following is a conversation with an AI. \n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you?\nHuman: "
 
 def transcribe_audio_to_text(filename):
-    recogizer=sr.Recognizer()
-    with sr.AudioFile(filename)as source:
-        audio=recogizer.record(source) 
-    try:
-        return recogizer.recognize_google(audio)
-    except:
-        print("skipping unkown error")
+            recogizer=sr.Recognizer()
+            with sr.AudioFile(filename)as source:
+                audio=recogizer.record(source) 
+            try:
+                return recogizer.recognize_google(audio)
+            except:
+                print("skipping unkown error")
+       # Define a function to generate a response using OpenAI's GPT-3
 
 def generate_response(prompt):
     response= openai.Completion.create(
@@ -38,69 +35,85 @@ def generate_response(prompt):
         stop=None,
         temperature=0.5,
     )
-    return response.choices[0].text
+    return response.choices[0].text      
 
-
-
-
+# Define a function called "speak_text" that takes in text as input
+def speak_text(text):
+    # Use the text-to-speech engine to read the text aloud
+    engine.say(text)
+    engine.runAndWait()
 
 
 def main():
 
+    # Create a Gradio interface for the chatbot
     with gr.Blocks() as demo:
 
-        gr.Markdown("""<h1><center>ChatGPT Voice Bot with OpenAI and Gradio Demo </center></h1>
-            """)
+        # Add a title to the interface
+        gr.Markdown("""<h1><center>ChatGPT Voice Bot with OpenAI and Gradio Demo </center></h1>""")
+
+        # Create a chatbot instance and a state object
         chatbot = gr.Chatbot()
         state = gr.State()
-        #msg = gr.Textbox(placeholder = prompt)
+
+        # Create a button to trigger audio recording
         rec_button = gr.Button(value="Record audio")
+
+        # Create a button to clear the chat history
         clear = gr.Button("Clear")
 
+        # Define a function to update the chat prompt when the record audio button is clicked
         def promt(rec_button):
             text = "Say something..."
             print(text)
             return gr.update(value=text, visible=True)
-        
+
+        # Define a function to record audio and transcribe it to text
         def rec(history):
             history = history or []
             filename ="input.wav"
             print("Start recording...")
+
+            # Use SpeechRecognition library to record audio from microphone
             with sr.Microphone() as source:
                 recognizer=sr.Recognizer()
                 source.pause_threshold=1
                 audio=recognizer.listen(source,phrase_time_limit=None,timeout=None)
 
+                # Save audio to file
                 with open(filename,"wb")as f:
                     f.write(audio.get_wav_data())  
-            #transcript audio to test 
+
+            # Transcribe audio to text
             text=transcribe_audio_to_text(filename)
             print("Recognized: ", text)
+
+            # Add user input to chat history
             history = history + [[text, None]]
-            return history, history, gr.update(value="Responding...", visible=True)
+            return history, history, gr.update(value="Responding...", visible=True)  
+        
 
-        def speak_text(text):
-            engine.say(text)
-            engine.runAndWait()
-
+        # Define a function called "bot" that takes in the chat history as input
         def bot(history):
-                history = history or []
-                print(history)
-                try:
-                    response = generate_response(history)    
-                    history[-1][1] = response
-                    time.sleep(3)
+            # If history is not defined, initialize an empty list
+            history = history or []
+            # Print the chat history for debugging purposes
+            print(history)
+            try:
+                # Generate a response from the OpenAI API using the chat history
+                response = generate_response(history)
+                # Add the response to the chat history
+                history[-1][1] = response
+                # Wait for 3 seconds to simulate typing time
+                time.sleep(3)
 
-                except Exception as e:
-                    print("An error ocurred : {}".format(e))
-                    history[-1][1] = str(e)
-                    return history, history, gr.update(value="Record audio", visible=True)
-                #read resopnse using GPT3
-                speak_text(response)
-
+            except Exception as e:
+                # If an error occurs, print the error message and add it to the chat history
+                print("An error occurred: {}".format(e))
+                history[-1][1] = str(e)
+                # Return the chat history and prompt the user to record audio again
                 return history, history, gr.update(value="Record audio", visible=True)
             
-
         #msg.submit(bot, inputs=state, outputs=[chatbot, state], queue=False).then(bot, inputs=state, outputs=[chatbot, state])
         
 
